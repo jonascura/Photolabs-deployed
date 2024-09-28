f3bb835d0327fb007a82cae98c763368d91eb9eb
Jonas Cura <jonasm.cura@gmail.com>
2024-09-27 19:08:59 -0600

    add pool to src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,25 @@ const ENV = require("./environment");
 
 const app = require("./application")(ENV);
 const server = require("http").Server(app);
+const { Pool } = require("pg");
+
+// Set up your PostgreSQL connection
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // Make sure you set this in your environment variables
+  ssl: {
+    rejectUnauthorized: false, // This might be necessary for Render
+  },
+});
+
+// Test the connection to PostgreSQL
+pool.connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL database.");
+  })
+  .catch(err => {
+    console.error("Error connecting to PostgreSQL server:", err);
+    process.exit(1); // Exit the application if connection fails
+  });
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT} in ${ENV} mode.`);
