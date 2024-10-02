b79583eaeb29baaaa47357454aa2971a09c751b7
Jonas Cura <jonasm.cura@gmail.com>
2024-10-02 03:48:42 -0600

    add logo fetch all

--- client/src/components/TopNavigationBar.jsx
+++ client/src/components/TopNavigationBar.jsx
@@ -9,7 +9,7 @@ const TopNavigation = ({ topics, favoritePhotos, getPhotosByTopicId, fetchAllPho
 
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo" onClick={handleLogoClick} >PhotoLabs</span>
+      <span className="top-nav-bar__logo" onClick={getPhotosByTopicId("")} >PhotoLabs</span>
       <TopicList topics={topics} getPhotosByTopicId={getPhotosByTopicId} />
       <FavBadge favoritePhotos={favoritePhotos} />
     </div>
