215dd4f9cc7e44264c54d3feee02d34bf2e79376
Jonas Cura <jonasm.cura@gmail.com>
2024-10-02 00:01:04 -0600

    remove infinite scroll

--- client/src/components/PhotoList.jsx
+++ client/src/components/PhotoList.jsx
@@ -1,44 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 const PhotoList = ({ photos, updateToFavPhotoIds, favoritePhotos, openModal, realPhotos, clickedPhoto }) => {
-  // console.log("inside Photolist", photos);
-  const [displayedPhotos, setDisplayedPhotos] = useState(photos.slice(0, 1));
-  const [hasMore, setHasMore] = useState(true);
-
-  const fetchMorePhotos = () => {
-    if (displayedPhotos.length >= photos.length) {
-      setHasMore(false);
-      return;
-    }
-    // load next photo batch
-    setDisplayedPhotos(photos.slice(0, displayedPhotos.length + 1));
-  }
+  console.log("inside Photolist", photos);
 
   // handle photos passed in from modal
-
   if (!photos) {
     photos=realPhotos.find(photo => photo.id === clickedPhoto.id).similar_photos;
     console.log("inside Photolist function", clickedPhoto, photos);
   }
 
   return (
-    <InfiniteScroll
-      dataLength={displayedPhotos.length}
-      next={fetchMorePhotos}
-      hasMore={hasMore}
-      loader={<h4>Loading...</h4>}
-      endMessage={<p>No more photos</p>}
-    >
     <ul className="photo-list">
       {photos.map(photo => (
         <PhotoListItem key={photo.id} photo={photo} updateToFavPhotoIds={updateToFavPhotoIds} favoritePhotos={favoritePhotos} openModal={openModal} />
       ))}
     </ul>
-    </InfiniteScroll>
   );
 };
 
